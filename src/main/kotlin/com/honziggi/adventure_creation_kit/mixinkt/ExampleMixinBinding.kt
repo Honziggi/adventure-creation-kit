110cf3696932d0c13f2af66ead213b311a8b6a1a
Honziggi <galiou@centrum.cz>
2025-05-31 14:33:20 +0200

    Trim project to only essential files

--- src/main/kotlin/com/honziggi/adventure_creation_kit/mixinkt/ExampleMixinBinding.kt
+++
@@ -1,9 +0,0 @@
-package com.honziggi.adventure_creation_kit.mixinkt
-
-//import net.minecraft.client.main.GameConfig
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
-//import java.io.File
-
-fun exampleMixinBinding(/*config: GameConfig, callbackInfo: CallbackInfo, file: File*/) {
-    println("Mixinkt : ExampleMixinBinding!")
-}
